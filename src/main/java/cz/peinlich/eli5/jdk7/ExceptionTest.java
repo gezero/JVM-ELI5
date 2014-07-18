836b2a55b8cad6f3da447348d955a1d2ac8fc8fe
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-07-18 09:03:22 +0200

    adding javadoc

--- src/main/java/cz/peinlich/eli5/jdk7/ExceptionTest.java
+++ src/main/java/cz/peinlich/eli5/jdk7/ExceptionTest.java
@@ -1,34 +1,29 @@
 package cz.peinlich.eli5.jdk7;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ExceptionTest
-{
+/**
+ * This class demonstrates how multiple exceptions can be cached in one catch block in JVM 7
+ * <p/>
+ * source: http://www.root.cz/clanky/novinky-v-nbsp-jdk-7-3-co-v-nbsp-jdk-7-naopak-nenajdeme/#ic=serial-box&icc=text-title
+ */
+public class ExceptionTest {
     public static List<String> log = new ArrayList<>();
 
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             int i = Integer.parseInt(args[0]);
-            System.out.format("1000 / %d = %d\n", i, 1000/i);
-        }
-        catch (final IndexOutOfBoundsException | NumberFormatException e)
-        {
+            System.out.format("1000 / %d = %d\n", i, 1000 / i);
+        } catch (final IndexOutOfBoundsException | NumberFormatException e) {
             System.err.println("You need to enter number as a first argument to the program");
             log.add(e.toString());
-        }
-        catch (ArithmeticException e)
-        {
+        } catch (ArithmeticException e) {
             System.err.println("Dividing by zero!");
             log.add(e.toString());
-        }
-        finally
-        {
+        } finally {
             System.out.println("\nError log: ");
-            for (String str : log)
-            {
+            for (String str : log) {
                 System.out.println(str);
             }
         }
